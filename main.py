9ebc8ee94a65b44851bdc1ff33c425094e801317
derletztename <derletztename@users.noreply.github.com>
2018-10-13 16:15:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -55,12 +55,13 @@ def image_filename():
 return filename
 
 
-position_x = device.get_current_position('x')
+'''position_x = device.get_current_position('x')
 position_y = device.get_current_position('y')filename = '{}/{}.jpg'.format(os.environ['IMAGES_DIR'], int(time.time()))
 ret = subprocess.call(['raspistill', '-w', '1024', '-h', '768', '-o', filename])
 if ret != 0:
    device.log('Problem getting image (error code: {}).'.format(ret), 'error')
-   sys.exit(1)
+  sys.exit(1)
+'''
    
    
 def upload_path(filename):
