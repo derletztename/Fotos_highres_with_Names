c08376d7a417facfcdef14d34ae10a5c7b92f8d0
derletztename <derletztename@users.noreply.github.com>
2018-10-13 15:32:39 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,88 @@
 """Fotos_highres_with_Names"""
 
+'''Take a photo using  Raspberry Pi camera.'''
+
+import os
+from time import time, sleep
+import json
+import requests
+import numpy as np
+import cv2
+
+
 from farmware_tools import device
 from farmware_tools import app
 import os, sys, time, subprocesspoints = app.get('points')
+
+def farmware_api_url():
+    major_version = int(os.getenv('FARMBOT_OS_VERSION', '0.0.0')[0])
+    base_url = os.environ['FARMWARE_URL']
+    return base_url + 'api/v1/' if major_version > 5 else base_url
+
+def log(message, message_type):
+    'Send a message to the log.'
+    try:
+        os.environ['FARMWARE_URL']
+    except KeyError:
+        print(message)
+    else:
+        log_message = '[take-photo] ' + str(message)
+        headers = {
+            'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+            'content-type': "application/json"}
+        payload = json.dumps(
+            {"kind": "send_message",
+             "args": {"message": log_message, "message_type": message_type}})
+        requests.post(farmware_api_url() + 'celery_script',
+                      data=payload, headers=headers)
+
+def rotate(image):
+    'Rotate image if calibration data exists.'
+    angle = float(os.environ['CAMERA_CALIBRATION_total_rotation_angle'])
+    sign = -1 if angle < 0 else 1
+    turns, remainder = -int(angle / 90.), abs(angle) % 90  # 165 --> -1, 75
+    if remainder > 45: turns -= 1 * sign  # 75 --> -1 more turn (-2 turns total)
+    angle += 90 * turns                   #        -15 degrees
+    image = np.rot90(image, k=turns)
+    height, width, _ = image.shape
+    matrix = cv2.getRotationMatrix2D((int(width / 2), int(height / 2)), angle, 1)
+    return cv2.warpAffine(image, matrix, (width, height))
+
+def image_filename():
+    'Prepare filename with timestamp.'
+    epoch = int(time())
+    filename = '{timestamp}.jpg'.format(timestamp=epoch)
+return filename
+
+
 position_x = device.get_current_position('x')
 position_y = device.get_current_position('y')filename = '{}/{}.jpg'.format(os.environ['IMAGES_DIR'], int(time.time()))
 ret = subprocess.call(['raspistill', '-w', '1024', '-h', '768', '-o', filename])
 if ret != 0:
    device.log('Problem getting image (error code: {}).'.format(ret), 'error')
    sys.exit(1)
+   
+   
+def upload_path(filename):
+    'Filename with path for uploading an image.'
+    try:
+        images_dir = os.environ['IMAGES_DIR']
+    except KeyError:
+        images_dir = '/tmp/images'
+    path = images_dir + os.sep + filename
+return path
+
+def rpi_camera_photo():
+    'Take a photo using the Raspberry Pi Camera.'
+    from subprocess import call
+    try:
+        filename_path = upload_path(image_filename())
+        retcode = call(
+            ["raspistill", "-w", "640", "-h", "480", "-o", filename_path])
+        if retcode == 0:
+            print("Image saved: {}".format(filename_path))
+        else:
+            log("Problem getting image.", "error")
+    except OSError:
+log("Raspberry Pi Camera not detected.", "error")
+
